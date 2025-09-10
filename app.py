abbb8aa1113f0c7483dee4cdf49d7c65e6c6f8f4
adrianamarcelahdz-cmd <adrianamarcelahdz@gmail.com>
2025-09-10 09:04:32 -0500

    Septiembre 10

--- app.py
+++ app.py
@@ -6,14 +6,35 @@ df = pd.read_csv(url, sep=';', encoding='utf-8')
 
 st.dataframe(df)
 
-#selected_columns = ['FECHA_HECHOS', 'DELITO', 'ETAPA', 'FISCAL_ASIGNADO', 'DEPARTAMENTO', 'MUNICIPIO']
-#df = df[selected_columns].sort_values(by='FECHA_HECHOS' )
+#Crea lista de las columnas que me interasan en su propio orden:
+selected_columns = ['FECHA_HECHOS', 'DELITO', 'ETAPA', 'FISCAL_ASIGNADO', 'DEPARTAMENTO', 'MUNICIPIO_HECHOS']
+#Actualizar el dtaframe -df- con las columnas de interes ordendas por fecha y reseteo de indice: 
+df = df[selected_columns].sort_values(by='FECHA_HECHOS', ascending=True). reset_index(drop=True)
+
+#Convertir fecha object a fecha 
+df['FECHA_HECHOS'] = pd.to_datetime(df['FECHA_HECHOS'], errors='coerce')
+
+df_serie_tiempo = df.copy()
+#Extraigo solo la fecha sin hora
+df['FECHA_HECHOS'] = df['FECHA_HECHOS'].dt.date
+st.dataframe(df)
+
+#Cálculo de los municipio con mas delitos 
+#.upper() para poner en mayuscula 
+max_municipio = df ['MUNICIPIO_HECHOS'].value_counts().index[0].upper()
+st.write(max_municipio)
+
+max_cantidad_municipio = df ['MUNICIPIO_HECHOS'].value_counts().iloc[0]
+#st.write(f'## Cantidad de Eventos: {max_cantidad_municipio}')
+
 
 #CONSTRUIR LA PÁGINA
-#st.set_page_config(page_title="Dashboard de Delitos - Fiscalía" layout=centered) 
+st.set_page_config(page_title="Dashboard de Delitos - Fiscalía", layout="centered") 
 #st.header("Dashboard de Delitos - Fiscalía")
-#st.dataframe(df)
+st.dataframe(df)
 
-st.subheader("Tipo de Delito")
-delitos = df['DELITO'].value_counts()
-st.bar_chart(delitos)
+st.write(f"## Municipio con más delitos: {max_municipio} con {max_cantidad_municipio} reportes")
+
+#st.subheader("Tipo de Delito")
+#delitos = df['DELITO'].value_counts()
+#st.bar_chart(delitos)
